36901f8b9734a834354b6f28101ce55690b9d18a
Victor Manuel Cervantes <vicmns@gmail.com>
2014-04-18 18:10:18 -0500

    Added RSS item Image to Widget list.

--- RSSReader/src/com/vicmns/rssreader/widget/WidgetListProvider.java
+++ RSSReader/src/com/vicmns/rssreader/widget/WidgetListProvider.java
@@ -4,8 +4,15 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.ImageSize;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
 import com.vicmns.rssreader.R;
 import com.vicmns.rssreader.activities.RssItemDetailsActivity;
 import com.vicmns.rssreader.models.RssItem;
@@ -14,10 +21,14 @@ import com.vicmns.rssreader.models.RssItems;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService.RemoteViewsFactory;
 
@@ -25,12 +36,29 @@ public class WidgetListProvider implements RemoteViewsFactory {
 	private Context context;
 	private List<RssItem> rssItemList;
 	private int appWidgetId;
+	
+	protected ImageLoader imageLoader = ImageLoader.getInstance();
+	private DisplayImageOptions options;
+	private ImageSize targetImageSize;
+	
+	private Bitmap bitmap;
 
 	public WidgetListProvider(Context context, Intent intent) {
 		this.context = context;
 		
 		rssItemList = new ArrayList<RssItem>();
 		Bundle bundle = intent.getBundleExtra("AdapterBundle");
+		
+		options = new DisplayImageOptions.Builder()
+		.showImageOnLoading(R.color.grey)
+		.showImageForEmptyUri(R.color.grey)
+		.cacheInMemory(true)
+		.cacheOnDisc(true)
+		.considerExifParams(true)
+		.bitmapConfig(Bitmap.Config.ARGB_8888)
+		.build();
+		
+		targetImageSize = new ImageSize(70, 70);
 
 		if(bundle != null) {
 			appWidgetId = bundle.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
@@ -67,6 +95,8 @@ public class WidgetListProvider implements RemoteViewsFactory {
 		remoteView.setTextViewText(R.id.rss_list_item_date_textView,
 				df.format(rssItemList.get(position).getPubDate()));
 		
+		setImageBitmap(position, remoteView);
+		
 		Intent intent = new Intent(context, RssItemDetailsActivity.class);
 		intent.putExtra("RssItem", rssItemList.get(position));
 		intent.setAction(Intent.ACTION_SEND);
@@ -74,6 +104,12 @@ public class WidgetListProvider implements RemoteViewsFactory {
         remoteView.setOnClickFillInIntent(R.id.rss_list_item_layout, intent);
 
 		return remoteView;
+	}
+	
+	private void setImageBitmap(final int position, final RemoteViews remoteView) {
+		bitmap = imageLoader.loadImageSync(rssItemList.get(position).getImageURL(), targetImageSize, options);
+		
+		remoteView.setImageViewBitmap(R.id.rss_list_item_imageView, bitmap);
 	}
 
 	@Override
